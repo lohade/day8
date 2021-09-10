86a3c8fd3ee6dc4b7b0db9d33710242384dedb81
lohade <abhisheklohade8584@gmail.com>
2021-09-10 19:58:32 +0530

    third case

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ import java.util.Scanner;
 
 public class Main {
 
-
+    static void compareTo(double value1,double value2){
+        if(value1>value2){
+            System.out.println("length of first line is greater");
+        }
+        else if(value1<value2){
+            System.out.println("length of second line is greater");
+        }
+        else{
+            System.out.println("length of both line is equal");
+        }
+    }
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Welcome to Line Comparison Computation Program");
@@ -29,6 +39,9 @@ public class Main {
 
         System.out.println("length of line 1:"+length_of_line1);
         System.out.println("length of line 2:"+length_of_line2);
+
+        System.out.println();
+        compareTo(length_of_line1,length_of_line2);
         if(length_of_line1==length_of_line2){
             System.out.println("length of Line is equal");
 
