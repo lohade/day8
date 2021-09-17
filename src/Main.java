827fb4b05f4f6619d54e1ae001c1ed0ebd69e8b8
lohade <abhisheklohade8584@gmail.com>
2021-09-17 10:51:03 +0530

    comaparing line

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,45 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+        // write your code here
         System.out.println("Welcome to Line Comparison Computation Program");
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter first coordinate:");
+        System.out.println("enter first coordinate of first line:");
         int x1=sc.nextInt();
         int y1=sc.nextInt();
-        System.out.println("enter second coordinate:");
+        System.out.println("enter second coordinate of first line:");
         int x2=sc.nextInt();
         int y2=sc.nextInt();
-        float square_length_of_x=(float)(Math.pow((x2-x1),2));
-        float square_length_of_y=(float)(Math.pow((y2-y1),2));
-        double length_of_line=Math.sqrt(square_length_of_x+square_length_of_y);
-        System.out.println("length of line:"+length_of_line);
+        //find length of line
+        double length_of_line1=Math.sqrt((double)(Math.pow((x2-x1),2))+(double) Math.pow((y2-y1),2));
+
+
+        System.out.println("enter first coordinate of second line:");
+        int x3=sc.nextInt();
+        int y3=sc.nextInt();
+        System.out.println("enter second coordinate of second line:");
+        int x4=sc.nextInt();
+        int y4=sc.nextInt();
+        //find length of line2
+        double length_of_line2=Math.sqrt((double)(Math.pow((x4-x3),2))+(double) Math.pow((y4-y3),2));
+
+
+
+
+        System.out.println("length of line 1:"+length_of_line1);
+        System.out.println("length of line 2:"+length_of_line2);
+        //compare line of length
+        if(length_of_line1==length_of_line2){
+            System.out.println("length of Line is equal");
+
+        }
+        else{
+            System.out.println("length of line is not equal");
+        }
+
+
     }
 }
 
