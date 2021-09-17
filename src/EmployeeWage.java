98d9b2079eaaf6f9a637bfa0c323645800642e08
lohade <abhisheklohade8584@gmail.com>
2021-09-17 08:48:07 +0530

    employee is present or absent

---
+++ src/EmployeeWage.java
@@ -0,0 +1,13 @@
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to employee wage problem");
+        int a=(int)Math.floor(Math.random()*10) %2;
+        if (a == 1) {
+            System.out.println("Employee is present");
+        }
+        else {
+            System.out.println("employee is absent");
+        }
+    }
+}
