cbfda0f3e3a9e6cc550ecc39a9cdaeba9b924e2b
lohade <abhisheklohade8584@gmail.com>
2021-09-17 10:54:38 +0530

    compare to method

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ import java.util.Scanner;
 
 public class Main {
 
-
+    static void compareTo(double value1,double value2){
+        if(value1>value2){
+            System.out.println("length of first line is greater");
+        }
+        else if(value1<value2){
+            System.out.println("length of second line is greater");
+        }
+        else{
+            System.out.println("length of both line is equal");
+        }
+    }
     public static void main(String[] args) {
         // write your code here
         System.out.println("Welcome to Line Comparison Computation Program");
@@ -13,7 +23,6 @@ public class Main {
         System.out.println("enter second coordinate of first line:");
         int x2=sc.nextInt();
         int y2=sc.nextInt();
-        //find length of line
         double length_of_line1=Math.sqrt((double)(Math.pow((x2-x1),2))+(double) Math.pow((y2-y1),2));
 
 
@@ -23,15 +32,16 @@ public class Main {
         System.out.println("enter second coordinate of second line:");
         int x4=sc.nextInt();
         int y4=sc.nextInt();
-        //find length of line2
-        double length_of_line2=Math.sqrt((double)(Math.pow((x4-x3),2))+(double) Math.pow((y4-y3),2));
+        int length_of_line2=(int)Math.sqrt((Math.pow((x4-x3),2))+ Math.pow((y4-y3),2));
 
-
+        //float length_of_line=Math.sqrt(square_length_of_x+square_length_of_y);
 
 
         System.out.println("length of line 1:"+length_of_line1);
         System.out.println("length of line 2:"+length_of_line2);
-        //compare line of length
+
+        System.out.println();
+        compareTo(length_of_line1,length_of_line2);
         if(length_of_line1==length_of_line2){
             System.out.println("length of Line is equal");
 
@@ -43,7 +53,6 @@ public class Main {
 
     }
 }
-
 
 
 
